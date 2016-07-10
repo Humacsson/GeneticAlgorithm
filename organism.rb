50d973e4309bb479dd3cbe0d1ba73f9efeef709b
Johan Näslund <Humacsson@users.noreply.github.com>
2016-07-10 15:30:12 +0900

    WIP

--- organism.rb
+++ organism.rb
@@ -5,7 +5,7 @@ class Organism
 	def initialize random_generator
 		@random_generator = random_generator
 	end
-	
+
 	def fitness
 		fail NotImplementedError
 	end
@@ -28,13 +28,19 @@ class Organism
 	def mutate chromosomes
 		mutation_index = @random_generator.rand(chromosomes.length)
 
-		chromosomes[mutation_index].mutate!
+		chromosome = chromosomes[mutation_index]
+		old_value = chromosome.value
+		chromosome.mutate!
+
+		p "Mutate chromsome #{ chromosome.variable_name } from #{ old_value } to #{ chromosome.value }"
 
 		chromosomes
 	end
 
 	def mutate!
 		self.chromosomes = mutate chromosomes
+
+		from_chromosomes!
 
 		self
 	end
@@ -56,13 +62,21 @@ class Organism
 	private
 
 	def self.one_point_crossover random_generator, mother, father
+		p "Performing crossover:"
 		crossover_point = random_generator.rand(mother.chromosomes.length)
+		p "Crossover point: #{ crossover_point }"
 
+		p "Mother: #{ mother.to_s }"
+		p "Father: #{ father.to_s }"
 		children = []
 		children << father.chromosomes[0..crossover_point] + mother.chromosomes[crossover_point+1..chromosome_length-1]
 		children << mother.chromosomes[0..crossover_point] + father.chromosomes[crossover_point+1..chromosome_length-1]
 
 		children.map! {|child| from_chromosomes(random_generator, child).mutate! }
+
+		children.each do |child|
+			p "Child: #{ child.to_s }"
+		end
 
 		children
 	end
